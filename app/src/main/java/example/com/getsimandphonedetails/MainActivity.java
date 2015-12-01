56a88be6817edb1f5a78824a66d8531728fcb36a
Abdul Rahman <uniquelyislam@gmail.com>
2015-12-01 12:46:03 +0800

    Initial commit

---
+++ app/src/main/java/example/com/getsimandphonedetails/MainActivity.java
@@ -0,0 +1,40 @@
+package example.com.getsimandphonedetails;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TelephonyInfo telephonyInfo = TelephonyInfo.getInstance(this);
+
+        String imsiSIM1 = telephonyInfo.getImsiSIM1();
+        String imsiSIM2 = telephonyInfo.getImsiSIM2();
+
+        boolean isSIM1Ready = telephonyInfo.isSIM1Ready();
+        boolean isSIM2Ready = telephonyInfo.isSIM2Ready();
+
+        boolean isDualSIM = telephonyInfo.isDualSIM();
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(" IME1 : " + imsiSIM1 + "\n" +
+                " IME2 : " + imsiSIM2 + "\n" +
+                " IS DUAL SIM : " + isDualSIM + "\n" +
+                " IS SIM1 READY : " + isSIM1Ready + "\n" +
+                " IS SIM2 READY : " + isSIM2Ready + "\n");
+
+        TelephonyManager mTelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imsi = mTelephonyMgr.getSubscriberId();
+
+        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        tv2.setText(imsi);
+
+    }
+}
